61076a0daaad244347efc61b01cf173dec68b8d1
Greg <solary311@gmail.com>
2019-09-21 15:54:33 +0300

    Update main.cpp

--- YYY/main.cpp
+++ YYY/main.cpp
@@ -1,15 +1,120 @@
-//
-//  main.cpp
-//  YYY
-//
-//  Created by Григорий on 21/09/2019.
-//  Copyright © 2019 JQ Software. All rights reserved.
-//
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+
+//3 Глава 1 я программа
+//int main(){
+//  const int Fut_1 = 12;
+//cout<<"Введите свой рост:_____"<<endl;
+//int duim;
+//cin>> duim;
+
+//float fut_R, duim_R;
+//fut_R=duim/Fut_1;
+//duim_R=duim % Fut_1;
+
+//cout<<"Ваш рост: "<<fut_R<<" футов и "<<duim_R<<"дюймов"<<endl;
+//return 0;
+//}
+
+
+
+
+//3я программа 3  главы
+//int main(){
+//  int DistanceKm;
+//  int VolumeGas;
+// cout<<"Введите про йденное расстояние в километрах:  "<<endl;
+//cin>>DistanceKm;
+///cout<< "Введите количество израсхлдованных литров:  "<<endl;
+//cin>>VolumeGas;
+
+//количество лмторов на 100км
+//double NumberPer100 = VolumeGas*100/DistanceKm;
+//cout<<NumberPer100<<" литров на 100 километров\n";
+
+// Галлоны и мили
+//const double Mile = 1.6, Gallon = 3.785;
+//double MilesPerGallons =(DistanceKm*Gallon)/(VolumeGas*Mile);
+//cout<<"Колличество миль на галлон:  "<<MilesPerGallons<<endl;
+//return 0;
+//}
+
+
+
+
+// 2я программа 3ей главы
+//int main(){
+//  const double DperM = 0.0254;
+//const double FperKg = 2.2;
+//double HeighInFutsAndDuims;
+//cout<<"Введите ваш рост в футах и дюймах:  "<< endl;
+//cin>>HeighInFutsAndDuims;
+//double WeightInFuts;
+//cout<<"Введите ваш вес в футах:  "<<endl;
+//cin>>WeightInFuts;
+
+// double HeighInDuims = HeighInFutsAndDuims * 12;
+//double HeighInMetres = HeighInDuims * DperM;
+// double WeightInKg = WeightInFuts/FperKg;
+//double IMT = WeightInKg/(HeighInMetres * HeighInMetres);
+//cout<< "Ваше ИМТ:   "<<endl;
+//return 0;
+
+//}
+
+
+
+
+
+///////////////-------//////////
+//2я глава
+//1я программа
+//int main(){
+
+
+//cout<< "МЕня зовут: Гриша Зенков"<<endl;
+// cout<< "Мой адрес: г. Нижний Новгород, ул.Ванеева, 221"<<endl;
+//return 0;
+//}
+//2я программа
+//int main(){
+//  double Farl;
+//  double Yard;
+//  cout<< "Введите расстояние в фарлонгах:  "<<endl;
+//  cin>>Farl;
+//  cout<<"Расстояние в ярдах:  "<<Farl*220<<endl;
+//return 0;
+//}
+
+
+//3я программа
+//void func1();
+//void func2();
+//int main(){
+//    func1();
+//    func1();
+//    func2();
+//    func2();
+//    return 0;
+
+//}
+
+//void func1(){
+//    cout<<" Three blind nice"<<endl;
+//}
+//void func2(){
+//    cout<<"See how they run"<<endl;
+//}
+
+
+//4я программа
+//int main(){
+//    double Cels, Fars;
+//    cout<< "Введите температуру в Цельсиях:  \n";
+//   cin>>Cels;
+//   cout<<Cels<<"Температура в цельсиях равна"<<(Cels*1.8+32.0)<<"градусов по фарингейту\n"<<endl;
+//  return 0;
+//}
