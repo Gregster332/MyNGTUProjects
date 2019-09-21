0f9c6129569cbab02bde21e56e03e40fb9799b1b
Greg <solary311@gmail.com>
2019-09-21 15:53:55 +0300

    Initial Commit

---
+++ YYY/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  YYY
+//
+//  Created by Григорий on 21/09/2019.
+//  Copyright © 2019 JQ Software. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
