3eb77da350a4bccab4488a1bef8f5add1e221a00
Gregster332 <55060092+Gregster332@users.noreply.github.com>
2019-09-27 12:15:35 +0300

    Add files via upload

---
+++ задания_из_4_главы.cpp
@@ -0,0 +1,120 @@
+
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+//Урок 4 1я программа
+//int main(){
+  //  const int NAMESIZE = 20;
+    
+   // char first[NAMESIZE], last[NAMESIZE];
+    //int age;
+    //char grade;
+    
+    //cout<<"what is your first name?"<<endl;
+    //cin.getline(first, NAMESIZE);
+   // cout<<"what is your last name?"<<endl;
+   // cin.getline(last, NAMESIZE);
+   // cout<<"what letter grade do you deserve?"<<endl;
+   // cin>>grade;
+   // cout<<"what is your age?"<<endl;
+    //cin>>age;
+   // cout<<"Name: "<<last<<","<<first<<'\n'<<endl;
+    
+   // char adjusted_grade = 'F';
+  //  if (grade == 'A' || grade == 'a') adjusted_grade = 'B';
+//else if (grade == 'B' || grade == 'b') adjusted_grade = 'C';
+   // else if (grade == 'C' || grade == 'c') adjusted_grade = 'D';
+   // cout<<"Grade: "<<adjusted_grade<<'\n'<<endl;
+   // cout<<"Age: "<<age<<'\n'<<endl;
+  //  return 0;
+//}
+
+//2я программа
+//struct CandyBar{
+ //   string Name;
+ //   int kkal;
+  //  double weight;
+    
+//};
+
+//int main(){
+ //   CandyBar snack = {"Mocha Munch", 350, 2.3};
+ //   cout<< "Candy: "<<snack.Name<<endl;
+ //   cout<< snack.kkal<<"kkal"<<endl;
+ //   cout<<snack.weight<<"Weight "<<endl;
+ //   return 0;
+//}
+
+//3я программа
+//struct CandyBar{
+//   string Name;
+//    int kkal;
+//    double weight;
+    
+//};
+
+//int main(){
+//    CandyBar snack[3] = {"Mars", 350, 2.3, "Snickers", 290, 4.5, "Twix", 450, 3.8};
+//    cout<<"Candy 1: "<<snack[0].Name<<endl;
+//    cout<< snack[0].kkal<<"Kkal"<<endl;
+//    cout<<"Weight: "<<snack[0].weight<<endl;
+    
+//    cout<<"Candy 2: "<<snack[1].Name<<endl;
+//    cout<< snack[1].kkal<<"Kkal"<<endl;
+//    cout<<"Weight: "<<snack[1].weight<<endl;
+    
+//    cout<<"Candy 3: "<<snack[2].Name<<endl;
+//    cout<< snack[2].kkal<<"Kkal"<<endl;
+//    cout<<"Weight: "<<snack[2].weight<<endl;
+
+//return 0;
+//}
+
+//4я программа
+
+//struct Pizza
+//{
+ //   string NameOfComp;
+ //   double diam;
+ //   double weight;
+//};
+
+//int main()
+//{
+//    Pizza pizza;
+//    cout << "Name of company" << endl;
+//    getline (cin, pizza.NameOfComp);
+//    cout << "Diameter" << endl;
+//   cin >> pizza.diam;
+ //   cout << "weight" << endl;
+  //  cin >> pizza.weight;
+    
+   // cout << "Name of company: " << pizza.NameOfComp << endl;
+//    cout << "Diameter: " << pizza.diam << endl;
+//    cout << "weight: " << pizza.weight << endl;
+//}
+
+//5я программа
+
+//struct CandyBar{
+//     string Name;
+//     int kkal;
+//      double weight;
+    
+//    };
+
+//int main(){
+//    CandyBar *snack = new CandyBar;
+//    cout<<"Name of snack:  "<<endl;
+//    getline (cin, snack->Name);
+//    cout<<"Kkal in snack: "<<endl;
+//    cin>>snack->kkal;
+//    cout<<"Weight of snack:  "<<endl;
+//    cin>>snack->weight;
+//
+//    cout<<"Name of snack: "<<snack->Name<<endl;
+//    cout<<"Kkal in snack: "<<snack->kkal<<endl;
+//    cout<<"Weight of snack: "<<snack->weight<<endl;
+    //}
